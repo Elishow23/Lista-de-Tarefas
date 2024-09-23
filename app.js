42d75d1e35388b024d618983e649e18d0fc4e99e
Elias dos Santos Cosme <ecosme015@gmail.com>
2024-09-23 13:19:25 -0300

    retornando código com CREATE e READ

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 
 function adicionar(){
+    let section = document.getElementById("select-barra")
     let titulo = document.getElementById("titulo-tarefa").value
     lista.push(titulo);
     console.log(lista)
+
+    let barra = ''
+
+    for(let listas of lista){
+         
+        barra += `<div class="barra-lista">
+                        <input type="checkbox" id="statusTarefa">
+                        <label for="statusTarefa">${listas}  </label>
+                        <button>EDITAR</button>
+                        <button>EXCLUIR</button>
+                    </div>
+                </div>`
+    }
+
+    section.innerHTML = barra;
 
 }
