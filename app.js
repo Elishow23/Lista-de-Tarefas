5cb4a307d605dd1cece3bcca4245d5df74e41b7d
Elias dos Santos Cosme <ecosme015@gmail.com>
2024-09-25 17:14:21 -0300

    tentando fazer a parte delete

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
+class tarefa{
+    constructor(titulo, status){
+        this.titulo = titulo
+        this.status = status
+    }
+
+}
 
 function adicionar(){
+
     let section = document.getElementById("select-barra")
     let titulo = document.getElementById("titulo-tarefa").value
-    lista.push(titulo);
+    
+    const tar = new tarefa(titulo, )
+
+    lista.push(tar.titulo, tar.status)
     console.log(lista)
 
     let barra = ''
 
     for(let listas of lista){
          
-        barra += `<div class="barra-lista">
-                        <input type="checkbox" id="statusTarefa">
-                        <label for="statusTarefa">${listas}  </label>
-                        <button>EDITAR</button>
-                        <button>EXCLUIR</button>
-                    </div>
+        barra += `<div class="barra-lista" >
+                    <input type="checkbox" id="statusTarefa">
+                    <label for="statusTarefa">${listas}</label>
                 </div>`
     }
 
+
     section.innerHTML = barra;
+
+}
+
+function excluirTarefa(){
+
+    //let section = document.getElementById("select-barra")
+    let status = document.getElementById("statusTarefa")
+
+    if(status.checked){
+        console.log(1)
+    }else{
+       console.log(0)
+    }
+
+    /*let barra = ''
+
+    for(let listas of lista){
+         
+        barra += `<div class="barra-lista" >
+                        <input type="checkbox" id="statusTarefa">
+                        <label for="statusTarefa">${listas}</label>
+                </div>`
+    }*/
+
+
+    //section.innerHTML = barra;
 
 }
